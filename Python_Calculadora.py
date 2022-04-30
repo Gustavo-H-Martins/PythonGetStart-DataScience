d9bc023aac849a6e3fe925c4088c21fce76ab075
Gustavo Henrique Lopes Martins <gustavogalo22@hotmail.com>
2022-04-30 18:19:16 -0300

    Criando Calculadora

---
+++ Python_Calculadora.py
@@ -0,0 +1,16 @@
+# Calculadora em Python
+
+contador = 0
+operador = 0
+n1 = 0
+n2 = 0
+resultado = 0
+ciclo = 0
+print(f'{3* "^~^"} esta é uma calculadora de Python, me ajude a melhorá-la e tornar algo melhor ? {3* "^~^"}')
+
+while ciclo == 'N':
+    contador += 1
+    n1 += int(input(f'Insira aqui o {contador}º número:  '))
+    contador += 1
+    n2 += int(input(f' Insira agora o {contador}º número:  '))
+    

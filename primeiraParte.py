62ceeab302bf9d6bd3270e1807eb84a993f30de6
Gustavo Henrique Lopes Martins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2022-05-12 15:16:51 -0300

    P03
    relação de alunos aprovados:
    ---------------------------
    M02 Bob
    M13 Bill
    M14 Cris
    dtype: object

--- primeiraParte.py
+++ primeiraParte.py
@@ -16,7 +16,7 @@ while continua != "N" :
     #imprime as duas Series
     print(notas); print("---------------"); print(alunos)
    
-  elif parte == 2
+  elif parte == 2 :
     #P02: Propriedades básicas das Series
     #cria a Series "alunos"
     alunos = pd.Series({'M02':'Bob','M05':'Dayse','M13':'Bill','M14':'Cris','M19':'Jimi'})
@@ -38,8 +38,28 @@ while continua != "N" :
     print('tipo (type): ', alunos_tipo)
     print('dtype da Series:', alunos_dtype)
     print('dtype do vetor de rótulos:', alunos_idx_dtype)
+    """
+    Para encerrar a subseção, vamos mostrar a indexação
+    booleana, a terceira e última técnica para indexar Series. No
+    presente momento, realizaremos apenas uma introdução ao
+    assunto, que será revisitado e mais bem detalhado em commits
+    posteriores. Neste modo de indexação, subconjuntos de dados são
+    selecionados com base nos valores da Series (valores do vetor de
+    dados) e não em seus rótulos/índices.
+    """
+  elif parte == 3 :
+    #P03: Indexação booleana
+    #cria as Series "notas" e "alunos"
+    notas = pd.Series([7.6, 5.0, 8.5, 9.5, 6.4])
+    alunos = pd.Series({'M02':'Bob','M05':'Dayse','M13':'Bill','M14':'Cris','M19':'Jimi'})
+    #obtém os índices dos alunos aprovados
+    idx_aprovados = notas[notas >= 7].index
+    #imprime os alunos aprovados
+    print('relação de alunos aprovados:')
+    print('---------------------------')
+    print(alunos[idx_aprovados])
     
-else :
+   else :
     break
   continua = input("Deseja continuar visualizando as partes ? \n [S] - Sim \n [N] - Não \n : ").upper().strip()
   

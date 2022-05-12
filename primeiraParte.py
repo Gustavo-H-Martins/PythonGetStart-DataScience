e3b1dd16239b48cf17aa459f9b1e5addfd6ec064
Gustavo Henrique Lopes Martins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2022-05-11 21:37:00 -0300

    Update primeiraParte.py
    Iniciando data wrangling

--- primeiraParte.py
+++ primeiraParte.py
@@ -1,5 +1,9 @@
-import pandas as pd 
-
-cidade = pd.read_csv('C:\Users\Jessica e Gustavo\PycharmProjects\pythonProject\ProjetoPandas\pandas-master\countries.csv')
-
-print(cidade)
+import pandas as pd
+bandeiras= pd.read_csv(f"C:/Users/Jessica e Gustavo/PycharmProjects/pythonProject/ProjetoPandas/pandas-master/flags.csv")
+paises = pd.read_csv(f"C:/Users/Jessica e Gustavo/PycharmProjects/pythonProject/ProjetoPandas/pandas-master/countries.csv")
+tudojunto = pd.merge(bandeiras, paises,
+                     how="inner",
+ left_on="name", right_on="country")
+print(tudojunto)
+print(10*"---")
+print(bandeiras)

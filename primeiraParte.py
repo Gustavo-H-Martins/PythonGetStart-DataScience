9184738c21653977172b9b59c58535008471c73a
Gustavo Henrique Lopes Martins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2022-05-12 10:13:49 -0300

    Inclusão da 1ª parte
    Referências do livro : Pandas Python Data Wrangling para Ciência de Dados

--- primeiraParte.py
+++ primeiraParte.py
@@ -1,9 +1,23 @@
 import pandas as pd
-bandeiras= pd.read_csv(f"C:/Users/Jessica e Gustavo/PycharmProjects/pythonProject/ProjetoPandas/pandas-master/flags.csv")
-paises = pd.read_csv(f"C:/Users/Jessica e Gustavo/PycharmProjects/pythonProject/ProjetoPandas/pandas-master/countries.csv")
-tudojunto = pd.merge(bandeiras, paises,
-                     how="inner",
- left_on="name", right_on="country")
-print(tudojunto)
-print(10*"---")
-print(bandeiras)
+# Formatação do código para usar cada etapa do desenvolvimento nesta primeira parte
+
+continua = 0
+while continua != "N" :
+  parte = int(input("Escolha qual a parte do código você quer visualizar digite o número referente: \n P01: Olá Series! - [1] \n : " ))
+  
+  if parte == 1 :  
+    #P01: Olá Series!
+    #cria a Series notas
+    notas = pd.Series([7.6, 5.0, 8.5, 9.5, 6.4])
+    #cria a Series alunos
+    lst_matriculas = ['M02','M05','M13','M14','M19']
+    lst_nomes = ['Bob','Dayse','Bill','Cris','Jimi']
+    alunos = pd.Series(lst_nomes,index=lst_matriculas)
+    #imprime as duas Series
+    print(notas); print("---------------"); print(alunos)
+    
+  else :
+    break
+  continua = input("Deseja continuar visualizando as partes ? \n [S] - Sim \n [N] - Não \n : ").upper().strip()
+  
+print("Fim do Algorítimo")
